3537cf99d898163106a1ebc54de273efd8b6fb4d
Tushar <tusharbharane2001@gmail.com>
2023-04-18 13:33:14 +0530

    Optimized and Responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,10 @@ import { Box } from '@chakra-ui/react';
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import Feed from '../components/Feed';
 import SideDrawer from '../components/Miscellaneous/SideDrawer';
 import RequestList from '../components/Friends/RequestList';
 import { setSelectedChat } from '../State/ChatSlice';
+import FilterBox from '../components/FilterBox';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -26,14 +26,16 @@ const Home = () => {
             {user && <SideDrawer />}
             <Box
                 display='flex'
-                flexFlow={{ base: "row", md: "row-reverse" }}
-                justifyContent={{ base: "stretch", md: "right" }}
+                flexFlow={{ base: "column", md: "row" }}
+                // border='2px solid red'
+                justifyContent={{ base: "center", md: "right" }}
                 w="100%"
-                h="90vh"
+                h={{ base: "1200px", md: "91.5vh" }}
                 p="10px"
             >
+                <FilterBox />
                 <RequestList />
-                <Feed />
+
             </Box>
         </div>
     )
--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,9 +1,10 @@
-import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Stack, Text, Textarea, useToast } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Heading, Stack, Text, useToast } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
-import CommentModal from '../components/Miscellaneous/CommentModal';
+import Posts from '../components/Posts';
+import LikeComment from '../components/LikeComment';
 
 const PostDetail = () => {
     const postId = useParams();
@@ -101,89 +102,26 @@ const PostDetail = () => {
 
 
     return (
-        <Box w="100%" h="100vh" p="10px">
+        <Box display="flex" justifyContent={'center'} w="100%" h="1475px" p="10px">
             <Box
                 display="flex"
                 flexDir="column"
                 p={3}
                 bg="#F8F8F8"
-                ml="15%"
-                w="65%"
+                w={{ base: "100%", md: "60%" }}
                 h="100%"
                 borderRadius="lg"
                 overflowY="hidden"
+                boxShadow={'dark-lg'}
             >
                 {post ?
-                    <Card
-                        w="100%"
-                        key={post._id}
-                        bg="#E8E8E8"
-                        px={3}
-                        py={2}
-                    >
-                        <Box
-                            display={"flex"}
-                            justifyContent="space-between"
-                        >
-                            <Box w="100%">
-                                <CardHeader>
-                                    <Flex spacing='4'>
-                                        <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                                            <Avatar key={post.userId._id} name={post.userId.name} src={post.userId.pic} />
-
-                                            <Box>
-                                                <Heading size='sm'>{post.userId.name}</Heading>
-                                                <Text>Creator, Chakra UI</Text>
-                                            </Box>
-                                        </Flex>
-                                    </Flex>
-                                </CardHeader>
-                                <CardBody>
-                                    <Text as="b">{post.heading}</Text>
-                                    <Text mt={2}>
-                                        {post.content}
-                                    </Text>
-                                </CardBody>
-                            </Box>
-                            {post.pic ? <Image
-                                boxSize='300px'
-                                src={post.pic}
-                            /> : ""
-                            }
-                        </Box>
-
-                        <CardFooter
-                            display={'flex'}
-                            justifyContent='space-between'
-                        >
-                            <Box>
-                                <h3>Likes: {likes}</h3>
-                                <h3>Comments: {comments}</h3>
-                            </Box>
-                            <Box
-                                flexWrap='wrap'
-                                sx={{
-                                    '& > button': {
-                                        minW: '136px',
-                                    },
-                                }}
-                            >
-                                <Button
-                                    variant='solid'
-                                    mr={5}
-                                    colorScheme='pink'
-                                    onClick={() => handleLike(post._id)}
-                                >
-                                    Like
-                                </Button>
-                                <CommentModal postId={post._id} handleCommentFunction={() => { allComments() }} />
-                            </Box>
-                        </CardFooter>
-                    </Card> : "No Post"
+                    <Posts post={post} handleLike={handleLike} handleComment={allComments} />
+                    :
+                    "No Post"
                 }
 
                 {/* <LikeAndComments /> */}
-                <Box display="flex" justifyContent={"space-between"} w="100%" h="54%" p={3} bg="white">
+                <Box display="flex" justifyContent={"space-between"} w="100%" h="54%" py={3} bg="white">
 
                     <Box
                         id='likes'
@@ -192,53 +130,9 @@ const PostDetail = () => {
                         w="30%"
                         h="100%"
                         alignItems={"center"}
+                        border={'1px solid black'}
                     >
-
-                        <Box
-                            display="flex"
-                            flexDir="column"
-                            p={3}
-                            bg="#F8F8F8"
-                            // bg="gold"
-                            w="100%"
-                            h="80%"
-                            borderRadius="lg"
-                            overflowY="hidden"
-                        >
-                            <Text fontSize='2xl' align="center" mb="2">Likes</Text>
-                            {likedUser ? (
-                                <Stack overflowY="scroll">
-                                    {likedUser.map((user) => (
-                                        <Box
-                                            display="felx"
-                                            justifyContent="space-between"
-                                            bg={"#E8E8E8"}
-                                            color={"black"}
-                                            px={3}
-                                            py={1.5}
-                                            borderRadius="lg"
-                                            key={user._id}
-                                        // _hover={{
-                                        //     background: "brown"
-                                        // }}
-                                        >
-
-                                            <Flex spacing='4'>
-                                                <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                                                    <Avatar name={user.userId.name} src={user.userId.pic} />
-
-                                                    <Box>
-                                                        <Heading size='sm'>{user.userId.name}</Heading>
-                                                        <Text>Creator, Chakra UI</Text>
-                                                    </Box>
-                                                </Flex>
-                                            </Flex>
-                                        </Box>
-                                    ))}
-                                </Stack>
-                            ) : "No requests"
-                            }
-                        </Box>
+                        <LikeComment name={'Likes'} users={likedUser} count={likes} />
                     </Box>
 
                     <Box
@@ -248,59 +142,9 @@ const PostDetail = () => {
                         w="68%"
                         h="100%"
                         alignItems={"center"}
+                        border={'1px solid black'}
                     >
-
-                        <Box
-                            display="flex"
-                            flexDir="column"
-                            p={3}
-                            bg="#F8F8F8"
-                            // bg="gold"
-                            w="100%"
-                            h="80%"
-                            borderRadius="lg"
-                            overflowY="hidden"
-                        >
-                            <Text fontSize='2xl' align="center" mb="2">Comments</Text>
-                            {commentedUser ? (
-                                <Stack overflowY="scroll">
-                                    {commentedUser.map((user) => (
-                                        <>
-                                            <Box
-                                                bg={"#E8E8E8"}
-                                                color={"black"}
-                                                px={3}
-                                                py={2}
-                                                borderRadius="lg"
-                                                key={user._id}
-                                            // _hover={{
-                                            //     background: "brown"
-                                            // }}
-                                            >
-
-                                                <Flex spacing='4'>
-                                                    <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                                                        <Avatar src={user.userId.pic} />
-
-                                                        <Box display={"flex"} justifyContent="center" alignItems={"center"}>
-                                                            <Heading size='sm'>{user.userId.name}</Heading>
-                                                            <Text ml={2}>Creator, Chakra UI</Text>
-                                                        </Box>
-                                                    </Flex>
-                                                </Flex>
-
-                                                <Box
-                                                    p={2}
-                                                >
-                                                    {user.commentText}
-                                                </Box>
-                                            </Box>
-                                        </>
-                                    ))}
-                                </Stack>
-                            ) : "No requests"
-                            }
-                        </Box>
+                        <LikeComment name={'Comments'} users={commentedUser} count={comments} />
                     </Box>
                 </Box>
             </Box>
