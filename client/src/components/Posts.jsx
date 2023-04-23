92711ee6d416e74a2f6e2a79649d4846e86dca8b
Tushar <tusharbharane2001@gmail.com>
2023-04-23 22:51:33 +0530

    Added Time for User's Post

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,20 +1,56 @@
 import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Stack, Text, useToast } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CommentModal from './Miscellaneous/CommentModal';
 import { ViewIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 import UserBadgeItem from './UserAvatar/UserBadgeItem';
+import ProfileModal from "./Miscellaneous/ProfileModal"
 
 const Posts = ({ post, handleLike, handleComment, postDetailButton }) => {
     const navigate = useNavigate();
+    const [timeElapsed, setTimeElapsed] = useState('');
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            const time = getTimeElapsed(post.createdAt);
+            setTimeElapsed(time);
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, [post.createdAt]);
+
+    const getTimeElapsed = (createdAt) => {
+        if (!createdAt) {
+            return '';
+        }
+        const now = new Date();
+        const created = new Date(createdAt);
+        if (isNaN(created)) {
+            return '';
+        }
+        const diff = now - created;
+        const seconds = Math.floor(diff / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        if (days > 0) {
+            return `${days}d ago`;
+        } else if (hours > 0) {
+            return `${hours}h ago`;
+        } else if (minutes > 0) {
+            return `${minutes}m ago`;
+        } else {
+            return `${seconds}s ago`;
+        }
+    };
 
     return (
         <Card
             w="100%"
             key={post._id}
             bg="#E8E8E8"
-            // px={3}
-            // py={2}
             border={'1px solid black'}
         >
             <Box
@@ -26,26 +62,28 @@ const Posts = ({ post, handleLike, handleComment, postDetailButton }) => {
                         <Flex spacing='4'>
                             <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
 
-                                <Avatar
-                                    key={post.userId._id}
-                                    name={post.userId.name}
-                                    src={post.userId.pic}
-                                />
+                                <ProfileModal user={post.userId}>
+                                    <Avatar
+                                        key={post.userId._id}
+                                        name={post.userId.name}
+                                        src={post.userId.pic}
+                                        cursor={'pointer'}
+                                    />
+                                </ProfileModal>
 
                                 <Box>
                                     <Heading size='sm'>{post.userId.name}</Heading>
                                     <Text>{post.userId.headline}</Text>
                                 </Box>
                             </Flex>
+                            <Text>{timeElapsed}</Text>
                         </Flex>
                     </CardHeader>
                     <CardBody
                         pt={1}
                     >
                         <Text as="b">{post.heading}</Text>
-                        <Text mt={2} textAlign={'justify'}>
-                            {post.content}
-                        </Text>
+                        <Text mt={2} textAlign={'justify'} dangerouslySetInnerHTML={{ __html: post.content }} />
                         <Box justifyContent={{ base: 'center' }} alignItems={'center'} w="100%" display="flex" flexWrap="wrap" mt={5}>
                             {post.tags.map((tag, index) => (
                                 <UserBadgeItem
@@ -84,6 +122,7 @@ const Posts = ({ post, handleLike, handleComment, postDetailButton }) => {
                         colorScheme='pink'
                         _hover={{ color: 'black' }}
                         onClick={() => handleLike(post._id)}
+                        border={'1px solid black'}
                     >
                         Like
                     </Button>
@@ -95,7 +134,8 @@ const Posts = ({ post, handleLike, handleComment, postDetailButton }) => {
                         <Button
                             size={{ base: 'xs', sm: 'sm', md: 'md' }}
                             onClick={() => navigate(`/post/${post._id}`)}
-                            _hover={{ bg: "skyblue" }}
+                            bg='#F8F8F8'
+                            _hover={{ bg: "violet" }}
                         >
                             <ViewIcon fontSize="2xl" m={1} />
                         </Button>
